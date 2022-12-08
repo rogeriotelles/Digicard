21663ffe9feee9b36b6b5cdd07bacd96c6710c3c
rogeriotelles <119432983+rogeriotelles@users.noreply.github.com>
2022-12-08 13:11:42 -0300

    commit initial
    the first commit of the site

---
+++ api.js
@@ -0,0 +1,29 @@
+const input = document.querySelector('#input');
+const imgDigi = document.querySelector('#img-digi');
+const digiTitle = document.querySelector('.card-title');
+const digiText = document.querySelector('.card-text');
+
+const fetchAPI = async () => {
+  const response = await fetch('https://digimon-api.vercel.app/api/digimon'); 
+  const data = await response.json();
+
+  return data;
+}
+
+function digiAPI() {
+  input.addEventListener('change', async ({ target }) => {
+    const digiArr = await fetchAPI();
+
+    const find = digiArr.find(({ name }) => name.toLowerCase() ===
+    target.value.toLowerCase());
+
+    const { name, level, img } = find;
+
+    digiText.innerText = `${name} é um digimon do tipo ${level}`;
+    digiTitle.innerHTML = name;
+    imgDigi.src = img;
+
+  })
+}
+
+window.onload = () => digiAPI();
